ea34490b144416f600f6f1873afd5984e4e6a1b0
Max Valasek <78054390+maxwellvalasek@users.noreply.github.com>
2025-06-24 11:37:24 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (51.04%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hmap = {}
-        noo = nums
-        for i in range(len(nums)):
+        n = len(nums)
+        for i in range(n):
             if target - nums[i] in hmap:
-                return [hmap[target - nums[i]],i]
+                return [hmap[target-nums[i]],i]
             hmap[nums[i]] = i
